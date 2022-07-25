2896c6ba967eb980f6ef3b4902116d95e3cbc4e8
JudyJry <judyjry0127@gmail.com>
2022-07-25 17:43:34 +0800

    update

--- src/tsx/PageWorks.tsx
+++ src/tsx/PageWorks.tsx
@@ -1,6 +1,7 @@
 /// <reference types="react-scripts" />
 import React from "react";
 import data from "../json/works";
+import "bootstrap/js/dist/modal";
 
 interface WorkData {
     name: string;
@@ -24,10 +25,10 @@ export default function PageWorks(): JSX.Element {
                         {
                             data.map((d, i) => {
                                 return (
-                                    <WorkListItem
+                                    <WorkListItem2
                                         key={i}
                                         data={d}
-                                    ></WorkListItem>
+                                    ></WorkListItem2>
                                 );
                             })
                         }
@@ -37,10 +38,10 @@ export default function PageWorks(): JSX.Element {
             {
                 data.map((d, i) => {
                     return (
-                        <WorkDetail
+                        <WorkDetail2
                             key={i}
                             data={d}
-                        ></WorkDetail>
+                        ></WorkDetail2>
                     );
                 })
             }
@@ -154,5 +155,101 @@ function WorkMember(props: React.PropsWithChildren<{ name: string, position: str
             <span className="member-name">{props.name}</span>
             <span className="member-position">{props.position}</span>
         </li>
+    );
+}
+
+function WorkListItem2(props: React.PropsWithChildren<{ data: WorkData }>): JSX.Element {
+    return (
+        <button className="work-list-item" data-bs-toggle="modal" data-bs-target={"#" + props.data.name}>
+            <div className="item-img">
+                <img src={props.data.cover as unknown as string} alt="" />
+            </div>
+            <div className="item-info">
+                <h2 className="item-name">{props.data.name}</h2>
+                <p className="item-tag">{props.data.tag}</p>
+            </div>
+        </button>
+    );
+}
+
+function WorkDetail2(props: React.PropsWithChildren<{ data: WorkData }>): JSX.Element {
+    return (
+        <div className="work-modal modal fade" id={props.data.name} tabIndex={-1} aria-hidden="true">
+            <div className="modal-dialog modal-fullscreen">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="work-detail d-flex flex-lg-row flex-column justify-content-end align-items-start">
+                            <div id={"carouselWork-" + props.data.name} className="carouselWork carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-indicators">
+                                    {
+                                        props.data.img.map((d, i) => {
+                                            return (
+                                                <button
+                                                    key={i}
+                                                    type="button"
+                                                    data-bs-target={"#carouselWork-" + props.data.name}
+                                                    data-bs-slide-to={i}
+                                                    className={i == 0 ? "active" : ""}
+                                                    //aria-current={i == 0 ? "true" : "false"}
+                                                    aria-label={"Slide " + i}
+                                                ></button>
+                                            );
+                                        })
+                                    }
+                                </div>
+                                <div className="carousel-inner">
+                                    {
+                                        props.data.img.map((d, i) => {
+                                            return (
+                                                <div className={i == 0 ? "carousel-item active" : "carousel-item"} key={i}>
+                                                    <img src={d as unknown as string} className="img-fluid d-block"></img>
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                                <button className="carousel-control-prev" type="button" data-bs-target={"#carouselWork-" + props.data.name} data-bs-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Previous</span>
+                                </button>
+                                <button className="carousel-control-next" type="button" data-bs-target={"#carouselWork-" + props.data.name} data-bs-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <div className="work-info">
+                                <h1 className="work-name">{props.data.name}</h1>
+                                <ul className="member-list">
+                                    {
+                                        props.data.member.map((d, i) => {
+                                            return (
+                                                <WorkMember
+                                                    key={i}
+                                                    name={d["member-name"]}
+                                                    position={d.position}
+                                                ></WorkMember>
+                                            );
+                                        })
+                                    }
+                                </ul>
+                                <hr></hr>
+                                <div className="work-describe">
+                                    {typeof props.data.describe == "string" ?
+                                        <p>{props.data.describe}</p>
+                                        :
+                                        props.data.describe.map((d, i) => {
+                                            return (<p>{d}</p>);
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
